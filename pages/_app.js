1b249f244c9e73c96ec48f20a854f1edd8234448
Arkids <kimchangse21@gmail.com>
2026-04-08 02:44:58 +0900

    애널리틱스 수정-2

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,5 @@
 import '../styles/globals.css'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-GK5NP5RM19"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-GK5NP5RM19');
-        `}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GK5NP5RM19" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-GK5NP5RM19');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}

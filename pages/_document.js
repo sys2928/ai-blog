7d9f3a579cc743acddc18fa689185a6271b37919
Arkids <kimchangse21@gmail.com>
2026-04-08 02:59:12 +0900

    fix GA measurement ID

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-GK5NP5RM19');
+              gtag('config', 'G-GK5NPSRM19');
             `,
           }}
         />
